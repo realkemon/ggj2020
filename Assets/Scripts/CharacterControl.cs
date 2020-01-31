6c23317be06de27acd82697ba44c47833baaa1d3
Marius Laptop <marius.muehleck@gmx.de>
2020-01-31 23:54:26 +0100

    * Adds standard third person controller

---
+++ Assets/Scripts/CharacterControl.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+[RequireComponent(typeof(CapsuleCollider))]
+[RequireComponent(typeof(Animator))]
+public class CharacterControl : MonoBehaviour
+{
+    [SerializeField] float m_MovingTurnSpeed = 360;
+    [SerializeField] float m_StationaryTurnSpeed = 180;
+
+    Rigidbody m_Rigidbody;
+    Animator m_Animator;
+    float m_TurnAmount;
+    float m_ForwardAmount;
+
+    void Start()
+    {
+        m_Animator = GetComponent<Animator>();
+        m_Rigidbody = GetComponent<Rigidbody>();
+
+        m_Rigidbody.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationZ;
+    }
+
+    public void Move(Vector3 move)
+    {
+        if (move.magnitude > 1.0f)
+            move.Normalize();
+
+        move = transform.InverseTransformDirection(move);
+        m_TurnAmount = Mathf.Atan2(move.x, move.z);
+        m_ForwardAmount = move.z;
+
+        float turnSpeed = Mathf.Lerp(m_StationaryTurnSpeed, m_MovingTurnSpeed, m_ForwardAmount);
+        transform.Rotate(0, m_TurnAmount * turnSpeed * Time.deltaTime, 0);
+
+        transform.Translate(move * 0.12f);
+    }
+}
---
+++ Assets/Scripts/UserControl.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+[RequireComponent(typeof(CharacterControl))]
+public class UserControl : MonoBehaviour
+{
+    private CharacterControl m_Character;
+    private Transform m_Cam;
+    private Vector3 m_CamForward;
+    private Vector3 m_Move;
+
+    private void Start()
+    {
+        if (Camera.main != null)
+            m_Cam = Camera.main.transform;
+        else
+            Debug.LogWarning("Warning: no main camera found. Third person character needs a Camera tagged \"MainCamera\", for camera-relative controls.", gameObject);
+
+        m_Character = GetComponent<CharacterControl>();
+    }
+
+    private void FixedUpdate()
+    {
+        // read inputs
+        float h = CrossPlatformInputManager.GetAxis("Horizontal");
+        float v = CrossPlatformInputManager.GetAxis("Vertical");
+
+        // calculate camera relative direction to move:
+        m_CamForward = Vector3.Scale(m_Cam.forward, new Vector3(1, 0, 1)).normalized;
+        m_Move = v * m_CamForward + h * m_Cam.right;
+
+        // pass all parameters to the character control script
+        m_Character.Move(m_Move);
+    }
+}
